c0fcaae892d47e5a4b227249cffb9efe380f6d2a
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2024-03-08 23:12:57 +0900

    chap04_spec.lua に着手

--- spec/chap01_spec.lua
+++ spec/chap01_spec.lua
@@ -31,60 +31,60 @@ describe("命令型モデル", function()
           -- 次の状態に移る
           state = currentInstruction.next                    
         end 
-      end 
+      end
       return tape
     end
     -- #@range_end(turing)
   end)
   -- **リスト 1.2 チューリング機械の実行例 **
-  describe('チューリング機械の実行例', () => {
-    -- #### <a name="turing_demo"> **チューリング機械の挙動** </a>
-    -- ![チューリング機械の挙動](images/turing_succ.gif) 
-    it('チューリング機械でsucc関数を実装する', (next) => {
-      --[[
-       #@range_begin(turing_example_succ)
-       2進法で10、つまり10進法で2を表すテープ
-      ]]
-      var tape = { 
-        '0':'1',
-        '1':'0'
-      };
-      /* チューリング機械に与えるプログラム */
-      var program = {
-        'q0': {"1": {"write": "1", "move": 1, "next": 'q0'},
-        "0": {"write": "0", "move": 1, "next": 'q0'},
-        "B": {"write": "B", "move": -1, "next": 'q1'}},
-        'q1': {"1": {"write": "0", "move": -1, "next": 'q1'},
-        "0": {"write": "1", "move": -1, "next": 'q2'},
-        "B": {"write": "1", "move": -1, "next": 'q3'}},
-        'q2': {"1": {"write": "1", "move": -1, "next": 'q2'},
-        "0": {"write": "0", "move": -1, "next": 'q2'},
-        "B": {"write": "B", "move": 1, "next": 'q4'}},
-        'q3': {"1": {"write": "1", "move": 1, "next": 'q4'},
-        "0": {"write": "0", "move": 1, "next": 'q4'},
-        "B": {"write": "B", "move": 1, "next": 'q4'}}
-      };
-      /* #@range_end(turing_example_succ) */
-      expect(
-      -- **リスト 1.3 1を加えるチューリング機械の実行 **
-      /* #@range_begin(turing_example_succ_test) */
-      machine(program,     -- プログラム
-      tape,        -- テープ
-      'q0',        -- 初期状態
-      'q4')        -- 終了状態
-      /* #@range_end(turing_example_succ_test) */
-      ).to.eql(
-      /* #@range_begin(turing_example_succ_test_result) */
-      {
-        '-1': 'B',
-        '0': '1',
-        '1': '1',
-        '2': 'B'
-      }
-      /* #@range_end(turing_example_succ_test_result) */
-      );
-      next();
-    });
-  });
+  -- describe('チューリング機械の実行例', function()
+  --   -- #### <a name="turing_demo"> **チューリング機械の挙動** </a>
+  --   -- ![チューリング機械の挙動](images/turing_succ.gif) 
+  --   it('チューリング機械でsucc関数を実装する', (next) => {
+  --     --[[
+  --      #@range_begin(turing_example_succ)
+  --      2進法で10、つまり10進法で2を表すテープ
+  --     ]]
+  --     var tape = { 
+  --       '0':'1',
+  --       '1':'0'
+  --     };
+  --     /* チューリング機械に与えるプログラム */
+  --     var program = {
+  --       'q0': {"1": {"write": "1", "move": 1, "next": 'q0'},
+  --       "0": {"write": "0", "move": 1, "next": 'q0'},
+  --       "B": {"write": "B", "move": -1, "next": 'q1'}},
+  --       'q1': {"1": {"write": "0", "move": -1, "next": 'q1'},
+  --       "0": {"write": "1", "move": -1, "next": 'q2'},
+  --       "B": {"write": "1", "move": -1, "next": 'q3'}},
+  --       'q2': {"1": {"write": "1", "move": -1, "next": 'q2'},
+  --       "0": {"write": "0", "move": -1, "next": 'q2'},
+  --       "B": {"write": "B", "move": 1, "next": 'q4'}},
+  --       'q3': {"1": {"write": "1", "move": 1, "next": 'q4'},
+  --       "0": {"write": "0", "move": 1, "next": 'q4'},
+  --       "B": {"write": "B", "move": 1, "next": 'q4'}}
+  --     };
+  --     /* #@range_end(turing_example_succ) */
+  --     expect(
+  --     -- **リスト 1.3 1を加えるチューリング機械の実行 **
+  --     /* #@range_begin(turing_example_succ_test) */
+  --     machine(program,     -- プログラム
+  --     tape,        -- テープ
+  --     'q0',        -- 初期状態
+  --     'q4')        -- 終了状態
+  --     /* #@range_end(turing_example_succ_test) */
+  --     ).to.eql(
+  --     /* #@range_begin(turing_example_succ_test_result) */
+  --     {
+  --       '-1': 'B',
+  --       '0': '1',
+  --       '1': '1',
+  --       '2': 'B'
+  --     }
+  --     /* #@range_end(turing_example_succ_test_result) */
+  --     );
+  --     next();
+  --   });
+  -- end);
 end)
 
